1864f8cba0b87665f0e6e88d282fe0f359d240ca
omaramin1 <omar.amin.jem@gmail.com>
2026-01-27 21:04:02 -0500

    Add all 28 reps from Viper + Ahmad as admin

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -32,6 +32,7 @@ interface AuthState {
 // Rep roster - add new reps here
 // Email as username, ENM number as password
 const REP_ROSTER: Omit<Rep, 'created_at'>[] = [
+  // ADMINS
   {
     id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
     name: 'Omar',
@@ -41,20 +42,42 @@ const REP_ROSTER: Omit<Rep, 'created_at'>[] = [
     isAdmin: true
   },
   {
-    id: '00000000-0000-0000-0000-000000000001',
-    name: 'Demo User',
-    email: 'demo@emg.com',
-    enm_number: '0000',
-    active: true
+    id: 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb',
+    name: 'Ahmad Amin',
+    email: 'ahmad@emgllc.co',
+    enm_number: 'ENM001',
+    active: true,
+    isAdmin: true
   },
-  {
-    id: '11111111-1111-1111-1111-111111111111',
-    name: 'Felix',
-    email: 'felix@emg.com',
-    enm_number: '1001',
-    active: true
-  },
-  // Add more reps here
+  // REPS FROM VIPER
+  { id: 'enm115', name: 'Jaydev Solanki', email: 'jayskee1989@gmail.com', enm_number: 'ENM115', active: true },
+  { id: 'enm095', name: 'Vernon Thomas', email: 'vernt151@gmail.com', enm_number: 'ENM095', active: true },
+  { id: 'enm075', name: 'Jacob Berube', email: 'jakeberube01@gmail.com', enm_number: 'ENM075', active: true },
+  { id: 'enm101', name: 'Totiana Washington-Stevens', email: 'totianaw.fcb@gmail.com', enm_number: 'ENM101', active: true },
+  { id: 'enm088', name: 'Lejohn Windn', email: 'lejohnw@gmail.com', enm_number: 'ENM088', active: true },
+  { id: 'enm045', name: 'Dominic Shea', email: 'dominicshea99@gmail.com', enm_number: 'ENM045', active: true },
+  { id: 'enm050', name: 'Tyler Dessaure', email: 'tydesse@gmail.com', enm_number: 'ENM050', active: true },
+  { id: 'enm083', name: 'Sean Ivey', email: 'poisenivey@gmail.com', enm_number: 'ENM083', active: true },
+  { id: 'enm116', name: 'Quinton Palmer', email: 'cue_palmer@yahoo.com', enm_number: 'ENM116', active: true },
+  { id: 'enm064', name: 'Haseeb Uppal', email: 'haseebuppal123@gmail.com', enm_number: 'ENM064', active: true },
+  { id: 'enm106', name: 'Marques Haskins', email: 'marques.haskins212@gmail.com', enm_number: 'ENM106', active: true },
+  { id: 'enm103', name: 'Talan Alexander', email: 'talanalexander23@gmail.com', enm_number: 'ENM103', active: true },
+  { id: 'enm094', name: 'Shibela Goins', email: 'shibelagoins@gmail.com', enm_number: 'ENM094', active: true },
+  { id: 'enm086', name: 'Keiron Moore', email: 'kmoore0722@outlook.com', enm_number: 'ENM086', active: true },
+  { id: 'enm047', name: 'Steven Moore', email: 'mooresteven227@gmail.com', enm_number: 'ENM047', active: true },
+  { id: 'enm070', name: 'Tommy DeSocio', email: 'tommyaeg@emarketinggroup.co', enm_number: 'ENM070', active: true },
+  { id: 'enm039', name: 'Paul Oslica', email: 'pauloslica94@gmail.com', enm_number: 'ENM039', active: true },
+  { id: 'enm107', name: 'Babacar Niang', email: 'bniang006@gmail.com', enm_number: 'ENM107', active: true },
+  { id: 'enm084', name: 'Christian Sawyer', email: 'chrisjeansawyer@gmail.com', enm_number: 'ENM084', active: true },
+  { id: 'enm096', name: 'Aiden Harris', email: 'aidenjon.134@gmail.com', enm_number: 'ENM096', active: true },
+  { id: 'enm035', name: 'Malani Lamkin', email: 'malani.lamkin@icloud.com', enm_number: 'ENM035', active: true },
+  { id: 'enm069', name: 'Cameron Fahey', email: 'cam.fahey97@gmail.com', enm_number: 'ENM069', active: true },
+  { id: 'enm104', name: 'Giavanna Agostini', email: 'giavannamodeling@gmail.com', enm_number: 'ENM104', active: true },
+  { id: 'enm098', name: 'Parion Martin', email: 'parionmartin02@gmail.com', enm_number: 'ENM098', active: true },
+  { id: 'enm071', name: 'Kirubel Motuma', email: 'kirubem@gmail.com', enm_number: 'ENM071', active: true },
+  { id: 'enm076', name: 'Vincenzo Castronovo', email: 'vcastronovo16@gmail.com', enm_number: 'ENM076', active: true },
+  { id: 'enm093', name: 'Jamar Johnson', email: 'jboydjohnson38@gmail.com', enm_number: 'ENM093', active: true },
+  { id: 'enm067', name: 'Yusuf Kamara', email: 'princekamara2727@gmail.com', enm_number: 'ENM067', active: true },
 ]
 
 export const useAuthStore = create<AuthState>()(
