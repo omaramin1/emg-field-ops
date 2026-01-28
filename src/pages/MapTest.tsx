c04761e3ffe5acd1f6574a925ba06f94dbb66d4d
omaramin1 <omar.amin.jem@gmail.com>
2026-01-28 09:52:51 -0500

    Try Carto tiles

--- src/pages/MapTest.tsx
+++ src/pages/MapTest.tsx
@@ -39,8 +39,8 @@ export default function MapTest() {
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; Carto'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         <LocationMarker />
       </MapContainer>
